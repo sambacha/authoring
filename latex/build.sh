8508e74e6738cdfd58c73251cd47152bba2c4783
sam bacha <sam@freighttrust.com>
2020-08-17 19:54:24 -0700

    Create build.sh

---
+++ latex/build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -e
+
+ORG=companyName
+PAPER=somePaper
+
+cd $(dirname $0)/..
+
+upload_ci_artifact() {
+  echo --- artifact: $1
+  if [[ -r $1 ]]; then
+    ls -l $1
+    if ${BUILDKITE:-false}; then
+      (
+        set -x
+        buildkite-agent artifact upload $1
+      )
+    fi
+  else
+    echo ^^^ +++
+    echo $1 not found
+  fi
+}
+
+OK=true
+
+for tex in *.tex; do
+  echo --- $tex
+  (
+    set -x
+    ci/docker-run.sh evilmachines/texlive lualatex -interaction=nonstopmode -halt-on-error $tex
+  ) || OK=false
+  upload_ci_artifact ${tex%.*}.pdf
+  upload_ci_artifact ${tex%.*}.log
+  $OK || { echo Failure.; exit 1; }
+done
+
+echo Done.
+
+if [[ -z "$BUILDKITE_TAG" ]]; then
+  exit 0
+fi
+
+echo --- Uploading assets for $BUILDKITE_TAG
+ci/docker-run.sh evilmachines/texlive ci/upload-release-asset.sh $ORG $PAPER "$BUILDKITE_TAG" *.pdf
+
