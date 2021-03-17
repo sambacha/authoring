e461e264b4deed9e36b76feee80ef508e94b073a
sam bacha <sam@freighttrust.com>
2021-03-17 11:03:52 -0700

    feat: working

--- asciidoc/md-to-adoc.sh
+++ asciidoc/md-to-adoc.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
 echo "Starting conversion, you must have kramdoc installed"
+kramdoc --version
 echo "converting all md-gfm files into asciidoc format..."
 find ./ -name "*.md" -type f -exec sh -c \ 'kramdoc --format=GFM --wrap=ventilate --output={}.adoc {}' \;
