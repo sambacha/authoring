f2d926c2551a68b6e1273fd724fb47b9a6dfcae3
sam bacha <sam@manifoldfinance.com>
2022-05-25 19:38:21 -0700

    Update prettier.config.js

--- prettier.config.js
+++ prettier.config.js
@@ -1,9 +1,27 @@
 'use strict';
-
+// "schema": "https://raw.githubusercontent.com/SchemaStore/schemastore/master/src/schemas/json/prettierrc.json",
+// "$schema": "http://json.schemastore.org/prettierrc",
 module.exports = {
-  jsxBracketSameLine: true,
-  printWidth: 80,
+  arrowParens: 'always',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  printWidth: 100,
   singleQuote: true,
-  trailingComma: 'es5',
-  proseWrap: 'always',
+  tabWidth: 2,
+  trailingComma: 'all',
+  quoteProps: 'as-needed',
+  semi: true,
+  overrides: [
+    {
+      files: '*.md',
+      options: {
+        parser: 'markdown',
+        printWidth: 72,
+        tabWidth: 4,
+        useTabs: true,
+        singleQuote: false,
+        bracketSpacing: true,
+      },
+    },
+  ],
 };
