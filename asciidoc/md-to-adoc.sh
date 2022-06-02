43a14fe1cd8910d3d752dd0b715b82d8b4afb1e6
sam bacha <sam@manifoldfinance.com>
2022-06-01 19:08:50 -0700

    Update md-to-adoc.sh

--- asciidoc/md-to-adoc.sh
+++ asciidoc/md-to-adoc.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 echo "Ruby check"
 eval "$(rbenv init -)"
@@ -7,4 +7,6 @@ sleep 1
 echo "Starting conversion, you must have kramdoc installed"
 kramdoc --version
 echo "converting all md-gfm files into asciidoc format..."
-find ./ -name "*.md" -type f -exec sh -c \ 'kramdoc --format=GFM --wrap=ventilate --output={}.adoc {}' \;
+#find ./ -name "*.md" -type f -exec sh -c \ 'kramdoc --format=GFM --wrap=ventilate --output={}.adoc {}' \;
+find ./ -name "*.md" -type f -exec sh -c \ 'kramdoc --lazy-ids --auto-id-prefix=_ --auto-id-separator=_ --format=GFM --wrap=ventilate --output={}.adoc {}' \;
+exit 0
